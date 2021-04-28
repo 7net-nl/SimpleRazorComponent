81b7cdb4b5272314290727fe3a02009b2f0d6691
Visual Studio <fttcab@gmail.com>
2021-04-28 22:19:07 +0200

    Add project files.

---
+++ CustomRazor/Models/InputViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomRazor.Models
+{
+    public class InputViewModel
+    {
+        public InputViewModel()
+        {
+            User1 = "";
+            User2 = "";
+            UserText1 = "";
+            UserText2 = "";
+        }
+        public string User1 { get; set; }
+        public string User2 { get; set; }
+        public string UserText1 { get; set; }
+        public string UserText2 { get; set; }
+    }
+}
---
+++ CustomRazor/Models/SaveProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomRazor.Models
+{
+    public class SaveProduct
+    {
+        public string Save(string Product ="")
+        {
+            return $"Save : {Product}";
+        }
+    }
+}
